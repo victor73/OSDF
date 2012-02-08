7534367e9ff7b78dde31177db3a6661217b723c9
victor73 <victor73@3b592ccf-7ef9-4f94-ae98-41b7e10cd085>
2012-02-08 15:54:16 +0000

    Removed exit code that was there for testing. Default port for faster usage.

--- bin/new_couch_user.sh
+++ bin/new_couch_user.sh
@@ -1,50 +1,58 @@
 #!/bin/bash
 
-set -e
+DEFAULT_COUCHDB_PORT=5984
 
-while [ -z $COUCH_SERVER ]; do
-    read -p "Enter the CouchDB hostname: " COUCH_SERVER
+# Ask for the hostname or IP address of the CouchDB server.
+while [ -z $COUCHDB_SERVER ]; do
+    read -p "Enter the CouchDB hostname or IP address: " COUCHDB_SERVER
 done
 
-while [ -z $COUCH_PORT ]; do
-    read -p "Enter the CouchDB port: " COUCH_PORT
+# Ask for the port number of the CouchDB server.
+read -p "Enter the CouchDB port (default is $DEFAULT_COUCHDB_PORT): " COUCHDB_PORT
+if [ -z $COUCHDB_PORT ]; then
+    COUCHDB_PORT=$DEFAULT_COUCHDB_PORT
+fi
+
+# Ask for the administrators username.
+while [ -z $COUCHDB_ADMIN ]; do
+    read -p "Enter the CouchDB Admin username: " COUCHDB_ADMIN
 done
 
-while [ -z $COUCH_DB ]; do
-    read -p "Enter the CouchDB database name: " COUCH_DB
-done
-
-while [ -z $COUCH_ADMIN ]; do
-    read -p "Enter the CouchDB Admin username: " COUCH_ADMIN
-done
-
-while [ -z $COUCH_ADMIN_PW ]; do
+# Ask for the administrators password to CouchDB. Be careful not to echo it
+# to the terminal.
+while [ -z $COUCHDB_ADMIN_PW ]; do
     stty -echo
-    read -p "Enter the CouchDB Admin password: " COUCH_ADMIN_PW; echo
+    read -p "Enter the CouchDB Admin password (not visible when typed): " COUCHDB_ADMIN_PW; echo
     stty echo
 done
 
-read -p "Enter the new user's username: " NEW_COUCH_USER 
+# Ask for the new user's username
+while [ -z $NEW_COUCHDB_USER ]; do
+    read -p "Enter the new user's username: " NEW_COUCHDB_USER 
+done
 
-while [ -z $COUCH_PW ]; do
+# Ask for the new user's password. Again, be careful not to echo it
+# to the terminal.
+while [ -z $COUCHDB_PW ]; do
     stty -echo
-    read -p "Enter the new user's password: " COUCH_PW; echo
+    read -p "Enter the new user's password (not visible when typed): " COUCHDB_PW; echo
     stty echo
 done
 
-while [ -z $COUCH_ROLE ]; do
-    read -p "Enter the new user's role: " COUCH_ROLE
+# CouchDB has a notion of "roles" for users. Here we can assign the user to such a role.
+while [ -z $COUCHDB_ROLE ]; do
+    read -p "Enter the new user's role (can't be blank): " COUCHDB_ROLE
 done
 
 
 SALT=`openssl rand 16 | openssl md5`
-PW_HASH=`echo -n "${COUCH_PW}${SALT}" | openssl sha1`
+PW_HASH=`echo -n "${COUCHDB_PW}${SALT}" | openssl sha1`
 
 USER_JSON=$(cat <<JSON
-{ "_id":"org.couchdb.user:$NEW_COUCH_USER",
+{ "_id":"org.couchdb.user:$NEW_COUCHDB_USER",
   "type":"user",
-  "name":"$NEW_COUCH_USER",
-  "roles":["$COUCH_ROLE"],
+  "name":"$NEW_COUCHDB_USER",
+  "roles":["$COUCHDB_ROLE"],
   "password_sha":"$PW_HASH",
   "salt":"$SALT"
 }
@@ -54,8 +62,21 @@ JSON
 # Convert the JSON to a single line and collapse out excess white space
 USER_JSON=$(echo "$USER_JSON" |  tr '\n' ' ' | sed 's/  */\ /g')
 
-echo $USER_JSON
-exit
+echo "Creating user."
+CURL_OUT=`curl -s -X POST http://$COUCHDB_SERVER:$COUCHDB_PORT/_users -u $COUCHDB_ADMIN:$COUCHDB_ADMIN_PW \
+     -H "Content-Type: application/json" -d "$USER_JSON"`
 
-curl -X POST http://$COUCH_SERVER:$COUCH_PORT/_users -u $COUCH_ADMIN:$COUCH_ADMIN_PW \
-     -H "Content-Type: application/json" -d "$USER_JSON"
+CURL_RV=$?
+if [ "$CURL_RV" != "0" ]; then
+    echo "Problem creating account." >&2
+    exit 2
+else
+    ERROR_COUNT=`echo "$CURL_OUT" | grep -ci "[e]rror"`
+    if [ "$ERROR_COUNT" != "0" ]; then
+        echo "$CURL_OUT" >&2
+        exit 1
+    fi
+fi
+
+echo "Successfully created new user."
+exit 0
