454140d9c6b7b4a615e67ce14bd796521f2bd1ac
victor73 <victor73@3b592ccf-7ef9-4f94-ae98-41b7e10cd085>
2012-04-02 19:03:05 +0000

    Command line options added.

--- trunk/osdf.js
+++ trunk/osdf.js
@@ -1,18 +1,42 @@
 #!/usr/bin/node
 
-var commander = require('commander');
 var _ = require('underscore');
 var cluster = require('cluster');
 var osdf_utils = require('osdf_utils');
 var logger = osdf_utils.get_logger();
 var os = require('os');
 var user_count;
+// Flag to indicate whether a custom config file was specified
+var custom_config = false;
+// Flag to indicate whether a custom "working" directory was specified
+var custom_working = false;
+var config_path = null;
+var working_path = null;
 
 engine_start();
 
 function engine_start() {
     require('config');
-    var config = Config.get_instance(osdf_utils.get_config());
+    var commander = require('commander');
+    commander.option('-c, --config <path>', 'Specify a configuration file. Default is <OSDF_HOME>/conf/config.ini.')
+             .option('-w, --working <path>', 'Specify a path to the working directory where namespaces data is stored.')
+             .parse(process.argv);
+
+    config_path = commander.config;
+    working_path = commander.working;
+    if (config_path === null || typeof config_path === 'undefined') {
+        config_path = osdf_utils.get_config();
+    } else {
+        custom_config = true;
+    }
+
+    if (working_path === null || typeof working_path === 'undefined') {
+        // nothing
+    } else {
+        custom_working = true;
+    }
+
+    var config = Config.get_instance(config_path);
 
     if (cluster.isMaster) {
         console.log("OSDF_ROOT: " + osdf_utils.get_osdf_root());
@@ -69,12 +93,20 @@ function engine_start() {
 
     } else {
         var forked_worker = require("./worker");
-        forked_worker.start_worker(config);
+        forked_worker.start_worker(config, working_path);
     }
 }
 
 // Display server details when we have started up.
 function show_ready(address, port) {
+    if (custom_config) {
+        console.log("User specified configuration file: " + config_path);
+    }
+
+    if (custom_working) {
+        console.log("User specified working area: " + working_path);
+    }
+
     console.log("Total number of registered OSDF users: " + user_count);
     console.log("=================================");
     console.log("Welcome to");
